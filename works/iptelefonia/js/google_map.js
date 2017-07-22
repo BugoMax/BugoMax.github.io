f1dbaeb9cb7b34f8c3378350f4c4f44811639892
BugoMax <skami823@ukr.net>
2017-07-22 21:40:40 +0300

    Create index and contact files in new project

--- works/iptelefonia/js/google_map.js
+++ works/iptelefonia/js/google_map.js
@@ -5,13 +5,13 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+    var myLatlng = new google.maps.LatLng(50.491227, 30.492926);
     // 39.399872
     // -8.224454
-    
+
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 7,
+        zoom: 15,
 
         // The latitude and longitude to center the map (always required)
         center: myLatlng,
@@ -37,7 +37,7 @@ function init() {
             var p = data.results[0].geometry.location
             var latlng = new google.maps.LatLng(p.lat, p.lng);
             new google.maps.Marker({
-                position: latlng,
+                position: myLatlng,
                 map: map,
                 icon: 'images/loc.png'
             });
